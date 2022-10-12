2282c5af598ed7637cad116eadcc51809121f831
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-12 01:34:40 +0100

    build

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e;(e=document.querySelector(".textarea")).addEventListener("keydown",(t=>{"Backspace"===t.key&&1===e.children.length&&e.children[0].innerText<1&&t.preventDefault()}))})();
