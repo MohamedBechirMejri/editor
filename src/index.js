7628410142e3e5e667559b873a2b7a17ce73f2a8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-13 04:02:21 +0100

    make the overlay follow the caret

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+import getCaretCoords from "../libs/getCaretCoords";
 import keepLastElement from "../libs/keepLastElement";
 import togglePlaceholder from "../libs/togglePlaceholder";
 
 const textarea = document.querySelector(".textarea");
 
-textarea.addEventListener('input', e => {
-    document.querySelector('.block-picker').classList.toggle('open')
-})
+textarea.addEventListener("input", e => {
+  const overlay = document.querySelector(".block-picker");
+  const { x, y } = getCaretCoords();
+
+  overlay.classList.add("open");
+
+  overlay.style.position = "fixed";
+  overlay.style.top = y + 35 + "px";
+  overlay.style.left = x + 10 + "px";
+});
 
 keepLastElement(textarea);
 togglePlaceholder(textarea);
