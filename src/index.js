823615d7df67ee99f0bfc0ca11456e5b0c21fa35
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-13 01:24:32 +0100

    test overlay toggle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import togglePlaceholder from "../libs/togglePlaceholder";
 
 const textarea = document.querySelector(".textarea");
 
+textarea.addEventListener('input', e => {
+    document.querySelector('.block-picker').classList.toggle('open')
+})
+
 keepLastElement(textarea);
 togglePlaceholder(textarea);
