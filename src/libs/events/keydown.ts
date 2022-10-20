f1c307e21b24f3e22521153317832f6cedde0c18
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-20 09:21:39 +0100

    refactorcode

--- src/libs/events/keydown.ts
+++ src/libs/events/keydown.ts
@@ -1,3 +1,4 @@
+import blockInputOnFirstElement from "../blockInputOnFirstElement";
 import clickFirstBlock from "../clickFirstBlock";
 import filterBlocks from "../filterBlocks";
 import toggleOverlay from "../toggleOverlay";
@@ -7,26 +8,42 @@ const addKeydownListener = (textarea: HTMLElement, filter: string) => {
   const blocks: NodeList = document.querySelectorAll(".block");
 
   textarea.addEventListener("keydown", e => {
-    if (e.key === "/") toggleOverlay("open");
+    const { classList } = overlay!;
+    const { children } = textarea;
 
-    if (overlay!.classList.contains("open")) {
-      switch (true) {
-        case e.key.length === 1:
-          filter += e.key; // add letters or numbers only
-          break;
-        case e.key === "Backspace":
-          filter = filter.slice(0, -1); // delete last character
-          break;
-        case e.key === "Enter" || e.key === "Escape":
-          if (e.key === "Enter") clickFirstBlock(blocks);
-          filter = ""; // clear filter
-          e.preventDefault();
-          break;
-      }
+    switch (true) {
+      case e.key.length === 1:
+        filter += e.key; // add letters or numbers only
+        break;
 
-      if (!filter) toggleOverlay("close");
-      filterBlocks(blocks, filter);
+      case e.key === " ":
+        blockInputOnFirstElement(e, children);
+        break;
+
+      case e.key === "/":
+        toggleOverlay("open");
+        break;
+
+      case e.key === "Backspace":
+        blockInputOnFirstElement(e, children);
+        if (classList.contains("open")) filter = filter.slice(0, -1); // delete last character
+        break;
+
+      case e.key === "Escape":
+        filter = ""; // clear filter
+        e.preventDefault();
+        break;
+
+      case e.key === "Enter":
+        if (e.key === "Enter" && classList.contains("open"))
+          clickFirstBlock(blocks);
+        filter = ""; // clear filter
+        e.preventDefault();
+        break;
     }
+
+    if (!filter) toggleOverlay("close");
+    filterBlocks(blocks, filter);
   });
 };
 export default addKeydownListener;
