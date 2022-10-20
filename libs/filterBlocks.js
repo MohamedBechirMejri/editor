13861f854b212763a3ec8458bc24ada6a9efe0d8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-20 04:26:37 +0100

    clear filter on enbter

--- libs/filterBlocks.js
+++ libs/filterBlocks.js
@@ -1,10 +1,18 @@
+import toggleOverlay from "./toggleOverlay";
+
 const filterBlocks = (el, filter) => {
+  const overlay = document.querySelector(".block-picker");
+  const filterElement = document.getElementById("filter");
+
   el.addEventListener("keydown", e => {
-    const overlay = document.querySelector(".block-picker");
-    const filterElement = document.getElementById("filter");
+    if (e.key === "/") toggleOverlay(true);
 
     if (overlay.classList.contains("open")) {
       if (e.key.length === 1) filter += e.key;
+      if (e.key === "Enter" || e.key === "Escape") {
+        filter = "";
+        toggleOverlay();
+      }
       if (e.key === "Backspace") filter = filter.slice(0, -1);
       if (!filter) overlay.classList.remove("open");
 
