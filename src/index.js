bdd3067cf3f11d350e0f79c24f79e778aa539b45
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-20 04:25:56 +0100

    remove old toggle

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import addBlock from "../libs/addBlock";
 import filterBlocks from "../libs/filterBlocks";
 import keepLastElement from "../libs/keepLastElement";
 import setOverlayPosition from "../libs/setOverlayPosition";
-import toggleOverlay from "../libs/toggleOverlay";
 import togglePlaceholder from "../libs/togglePlaceholder";
 
 const textarea = document.querySelector(".textarea");
@@ -17,6 +16,5 @@ textarea.addEventListener("blur", e => e.target.focus());
 keepLastElement(textarea);
 togglePlaceholder(textarea);
 setOverlayPosition(textarea);
-toggleOverlay(textarea);
 addBlock(textarea);
 filterBlocks(textarea, filter);
