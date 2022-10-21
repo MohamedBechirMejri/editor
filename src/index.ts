ad414ed6cee4ff308988a918a9ee5be197654f63
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-21 07:55:53 +0100

    fix typos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./styles/main.scss";
 import addKeydownListener from "./libs/events/keydown";
-import addBlockClickListener from "./libs/events/blockClick";
+import addBlockClickListener from "./libs/events/clickBlock";
 import addInputListener from "./libs/events/input";
 
 const textarea: HTMLElement | null = document.querySelector(".textarea");
@@ -14,4 +14,5 @@ textarea!.addEventListener("blur", (e: any) => e.target.focus());
 
 addKeydownListener(textarea!, filter);
 addBlockClickListener();
+
 addInputListener(textarea!);
