1e7ac18526ab82cc3f315dfcc88136826f874677
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-22 09:22:57 +0100

    move old index to _app for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import addKeydownListener from "../lib/events/keydown";
+import addBlockClickListener from "../lib/events/clickBlock";
+import addInputListener from "../lib/events/input";
 
 const Editor = ({ Component, pageProps }: AppProps) => {
+  const textarea: HTMLElement | null = document.querySelector(".textarea");
+
+  let filter = "";
+
+  textarea!.focus();
+
+  // Keep editor focused
+  textarea!.addEventListener("blur", (e: any) => e.target.focus());
+
+  addKeydownListener(textarea!, filter);
+  addBlockClickListener();
+
+  addInputListener(textarea!);
+
   return <Component {...pageProps} />;
 };
 
