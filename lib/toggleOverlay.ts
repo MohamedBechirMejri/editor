964e3b595cd00e08675e2e84085d69bdb4ec92be
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-23 06:19:02 +0100

    remove redundant code

--- lib/toggleOverlay.ts
+++
@@ -1,7 +0,0 @@
-const toggleOverlay = (toggle: "open" | "close") => {
-  const overlay = document.querySelector(".block-picker");
-
-  if (toggle === "open") overlay!.classList.add("open");
-  else overlay!.classList.remove("open");
-};
-export default toggleOverlay;
